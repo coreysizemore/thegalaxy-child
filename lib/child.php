2006af4cc0a92a98b544b51430aa9f3dafbf2b27
Corey Sizemore <coreysizemore@gmail.com>
2018-06-27 16:52:54 -0700

    v1.0.2
    - Fixed minor parent theme name issue.

--- lib/child.php
+++ lib/child.php
@@ -6,7 +6,7 @@
 
 	function my_theme_enqueue_styles() {
 	
-	    $parent_style = 'alderaan-style';
+	    $parent_style = 'thegalaxy-style';
 	
 	    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
 	    wp_enqueue_style( 'child-style',
