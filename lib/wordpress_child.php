e129887fe648931fcaf2a5d3b3956585894250ce
Corey Sizemore <coreysizemore@gmail.com>
2022-03-07 08:22:00 -0800

    Update - 3/7/22
    Child theme refresh, updated files and added custom JS file.

--- lib/wordpress_child.php
+++ lib/wordpress_child.php
@@ -9,8 +9,8 @@
 	{
 		
 		wp_enqueue_style( 'galaxy-child-style', get_stylesheet_directory_uri() . '/style.css' );
-	    wp_enqueue_style( 'galaxy-child-custom-style', get_stylesheet_directory_uri() . '/css/custom.css' );
-	    wp_enqueue_script( 'galaxy-child-custom-js', get_stylesheet_directory_uri() . '/js/custom.js' );
+	    wp_enqueue_style( 'galaxy-child-custom-style', get_stylesheet_directory_uri() . '/css/custom.min.css' );
+	    wp_enqueue_script( 'galaxy-child-custom-js', get_stylesheet_directory_uri() . '/js/custom.min.js' );
 	    
 	}
 	
